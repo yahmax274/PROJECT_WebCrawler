90246cf51ab7828584d4b836eb5d3849973979ce
yahmax274 <111629917+yahmax274@users.noreply.github.com>
2022-11-25 20:47:23 +0800

    Add files via upload

--- 姚志朋/MultiprocessingTest5.py
+++ 姚志朋/MultiprocessingTest5.py
@@ -7,7 +7,6 @@ import numpy
 import openpyxl
 from openpyxl import load_workbook
 import concurrent.futures
-start = time.time() # 開始測量執行時間 
 #list
 type_list = []
 brand_list = []
@@ -18,16 +17,24 @@ spec_list = []
 act_list = []
 all_list = []
 Href_list=[]
+def WriteData():
+    #匯出csv檔
+    for i in range(len(Href_list)):
+        all = type_list[i]+"|"+brand_list[i]+"|"+name_list[i]+"|"+price_list[i]+"|"+date_list[i]+"|"+spec_list[i]+"|"+act_list[i]
+    all_list.append(all)
+    raw_data ={"app|big|mid|small|category|brand|item_name|market_price|sale_price|discount_price|date|item_specification|act": all_list}
+    df = pd.DataFrame(raw_data,columns=["app|big|mid|small|category|brand|item_name|market_price|sale_price|discount_price|date|item_specification|act"])
+    df.to_csv("momo1.csv",encoding='utf-8-Sig',index=False)
 def ReadData():
     fn = '3Ckey.xlsx'
-    wb = load_workbook( fn )   
-    # Work Sheet 
-    ws = wb.get_sheet_by_name( 'sheet2' )  
-    # Column 
-    column = ws[ 'A' ]   
+    wb = load_workbook( fn )
+    # Work Sheet
+    ws = wb.get_sheet_by_name( 'sheet2' )
+    # Column
+    column = ws[ 'A' ]
     mylist = [column[x].value for x in range(len(column))]
     return mylist
-def Main(Href):      
+def Main(Href):
     # url = "https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=10604406&sourcePageType=4"
     url =Href
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
@@ -47,7 +54,7 @@ def Main(Href):
             if type == []:
                 continue
             else:
-                for i in range(4):  
+                for i in range(4):
                     if type[i] == "":
                         type[i] ="null"
                 type = type[0]+"|"+type[1]+"|"+type[2]+"|"+type[3]+"|"+type[4]
@@ -77,7 +84,7 @@ def Main(Href):
                     if "市售價" in price[i]:
                         price_1 = price[i]
                         price_1 = price_1[3:]
-                        price_1 = price_1.replace("元","")   
+                        price_1 = price_1.replace("元","")
                         price_1 = price_1.replace(",","")
                     else:
                         price_1 = "null"
@@ -90,15 +97,15 @@ def Main(Href):
                         price_2 = price_2.replace("下單再折","")
                         price_2 = price_2.replace(",","")
                     else:
-                        price_2 = "null"  
+                        price_2 = "null"
                 if price_3 =="null" or price_3 =="":
                     if "折扣後價格" in price[i]:
                         price_3 = price[i]
                         price_3 = price_3[5:]
                         price_3 = price_3.replace("元賣貴通報","")
-                        price_3 = price_3.replace(",","") 
+                        price_3 = price_3.replace(",","")
                     else:
-                        price_3 = "null"  
+                        price_3 = "null"
             price = price_1+"|"+price_2+"|"+price_3
             price_list.append(price)
     #date
@@ -113,23 +120,19 @@ def Main(Href):
     act = "null"
     act_list.append(act)
 if __name__ == '__main__':
+    start = time.time() # 開始測量執行時間
     mylist=ReadData()
-    # Href=mylist[20]
-    # Main(Href)
     for link in mylist[0:10]:##如果網址內無商品，會跳錯誤訊息
         Href = link
         Href_list.append(Href)
         Main(Href)
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:executor.map(Main,Href) 
+    WriteData()
+    end = time.time() # 結束測量執行時間
+    print("執行時間為 %f 秒" % (end - start))
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:executor.map(Main,Href)
     # with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:executor.map(Main,Href, chunksize=6)
     # print(Href_list)
-#匯出csv檔
-for i in range(len(Href_list)):
-    all = type_list[i]+"|"+brand_list[i]+"|"+name_list[i]+"|"+price_list[i]+"|"+date_list[i]+"|"+spec_list[i]+"|"+act_list[i]
-    all_list.append(all)
-raw_data ={"app|big|mid|small|category|brand|item_name|market_price|sale_price|discount_price|date|item_specification|act": all_list}
-df = pd.DataFrame(raw_data,columns=["app|big|mid|small|category|brand|item_name|market_price|sale_price|discount_price|date|item_specification|act"])
-df.to_csv("momo1.csv",encoding='utf-8-Sig',index=False)
-end = time.time() # 結束測量執行時間
-print("執行時間為 %f 秒" % (end - start))
+
+
+
 
