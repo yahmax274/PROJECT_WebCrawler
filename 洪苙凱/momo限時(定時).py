bc78adcdc644ed5a232a2e99d5c004840c27d4ff
likai59 <118263461+likai59@users.noreply.github.com>
2022-11-28 20:54:11 +0800

    Add files via upload

--- 洪苙凱/momo限時(定時).py
+++ 洪苙凱/momo限時(定時).py
@@ -32,14 +32,14 @@ now = now.replace("-","/")
 now = now[:16]
 date = date_list[product_list[0]+1]
 if date[11:13] =="00":
-    while int(now[11:13])!= 11 or int(now[14:])<=50:
+    while int(now[11:13])!= 11 or int(now[14:])<50:
         print(now)
         time.sleep(10)
         now = str(datetime.now())
         now = now.replace("-","/")
         now = now[:16]
 else:
-    while int(now[11:13])-int(date[11:13])!= -1 or int(now[14:])<=50:
+    while int(now[11:13])-int(date[11:13])!= -1 or int(now[14:])<50:
         print(now)
         time.sleep(10)
         now = str(datetime.now())
